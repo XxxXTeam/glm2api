961183c924839ca8d5e75685541c7eddef69b8ed
MapleLeaf <197728340@qq.com>
2026-04-20 21:07:10 +0800

    feat: pyinstaller --onefile --paths ./src main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from pathlib import Path
 import sys
 
+# ====== 加路径 ======
 ROOT = Path(__file__).resolve().parent
 SRC = ROOT / "src"
 if str(SRC) not in sys.path:
     sys.path.insert(0, str(SRC))
+# ============================
 
+# ====== 导入 ======= 
+import glm2api.__main__
 from glm2api.__main__ import main
-
+# ============================
 
 if __name__ == "__main__":
     raise SystemExit(main())
